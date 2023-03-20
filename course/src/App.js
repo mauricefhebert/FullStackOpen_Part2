7be63958e9c289d35452ccfbbdd091c7b22eebfd
mauricefh <filiatreaultmaurice@gmail.com>
2023-03-20 12:10:37 -0400

    completed the reading material for rendering a collection, module

--- course/src/App.js
+++ course/src/App.js
@@ -1,9 +1,16 @@
-const App = () => {
+import Note from "./components/Note";
+
+const App = ({ notes }) => {
   return (
     <div>
-      <p></p>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
