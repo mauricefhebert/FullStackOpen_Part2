f8de925592f20ac5624d95f8caa5bbe304887b18
mauricefh <filiatreaultmaurice@gmail.com>
2023-03-23 00:55:20 -0400

    completed exercise 2.6 2.7

--- exercise/phonebook/src/App.js
+++ exercise/phonebook/src/App.js
@@ -4,19 +4,40 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (persons.find(x => x.name == newName)) {
+      alert(
+        `The name ${newName} is already in the application please use another name`
+      );
+      return;
+    }
+
+    const temp = [...persons];
+    const x = { name: newName };
+    temp.push(x);
+    setPersons(temp);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name:
+          <input value={newName} onChange={e => setNewName(e.target.value)} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        {persons.map(person => (
+          <p key={person.name}>{person.name}</p>
+        ))}
+      </div>
     </div>
   );
 };
